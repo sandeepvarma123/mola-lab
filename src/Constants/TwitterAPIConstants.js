15d301b221bf3627c67dbe78db4aac487b7c28b1
sandeepvarma <vatsavayi.sandeep@gmail.com>
2021-11-13 12:15:55 -0800

    added url

--- src/Constants/TwitterAPIConstants.js
+++ src/Constants/TwitterAPIConstants.js
@@ -1,4 +1,7 @@
 export const AUTH_TOKEN = "AAAAAAAAAAAAAAAAAAAAACn9VgEAAAAAoul2x7f0fHvjlhtBfTQ0tM2LutQ%3DBJct7Ecrh4j6wloXjxS3UTwVMphnlJtxUX0kdCot96bwKL5qAU"
-export const VALIDATE_USER_URL = '/api/users/by/username/';
-export const USERS_URL = '/api/users/'
+export const VALIDATE_USER_URL = 'https://api.twitter.com/2/users/by/username/';
+export const USERS_URL = 'https://api.twitter.com/2/users/'
 export const RETRIEVE_TWEETS_URL = '/tweets?max_results=10';
+
+
+//"proxy": "https://api.twitter.com/2/"
